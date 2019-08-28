27adc82debcc9c36c84a44437dbae132f497443d
ksunden <git@ksunden.space>
2019-08-27 23:19:48 -0500

    complete auth flow

--- yaqd_gdrive/_gdrive.py
+++ yaqd_gdrive/_gdrive.py
@@ -1,7 +1,10 @@
 import asyncio
+import functools
 import time
+import webbrowser
 
-import httpx
+import aiohttp
+import aiohttp.web
 import yaqd_core
 
 
@@ -14,7 +17,7 @@ def refresh_oauth(func):
         self._authorized = False
         try:
             await self._refresh_token()
-        except httpx.HTTPError:
+        except aiohttp.HTTPError:
             await self._authorize()
 
         return await func(self, *args, **kwargs)
@@ -32,7 +35,7 @@ class GDriveDaemon(yaqd_core.BaseDaemon):
     }
 
     def __init__(self, name, config, config_filepath):
-        self._http_client = httpx.AsyncClient()
+        self._http_session = aiohttp.ClientSession()
         self._upload_queue = {}
         self._id_mapping = {}
         self._free_ids = []
@@ -50,28 +53,57 @@ class GDriveDaemon(yaqd_core.BaseDaemon):
         self._update_file_url = config["update_file_url"]
 
     async def _authorize(self):
+        code = asyncio.Future()
+        port=9004
+        url = f"{self._authorization_url}?scope={'%20'.join(self._scopes)}&response_type=code&redirect_uri=http://127.0.0.1:{port}&client_id={self._client_id}"
+        print(url)
+
+        app = aiohttp.web.Application()
+
+        async def complete_auth(request):
+            nonlocal code, url
+            if "code" in request.query:
+                code.set_result(request.query["code"])
+                return aiohttp.web.Response(text="Authorization complete")
+            else:
+                return aiohttp.web.Response(
+                    text=f"Authorization failed\n{request.query.get('error', '')}\n{url}"
+                )
+
+        app.add_routes([aiohttp.web.get("/", complete_auth)])
+
+        webbrowser.open(url)
+
+        runner = aiohttp.web.AppRunner(app)
+        await runner.setup()
+        site = aiohttp.web.TCPSite(runner, "localhost", port)
+        await site.start()
+        code = await code
+        await runner.cleanup()
+        print(code)
 
         await self._obtain_token(code)
 
     async def _obtain_token(self, code):
-        res = await self._http_client.post(
+        async with self._http_session.post(
             self._token_url,
-            params={
+            json={
                 "code": code,
                 "client_id": self._client_id,
                 "client_secret": self._client_secret,
-                "redirect_uri": "https://127.0.0.1:9004",
+                "redirect_uri": "http://127.0.0.1:9004",
                 "grant_type": "authorization_code",
             },
-        )
-        res.raise_for_status()
-        json = res.json()
-        self._access_token = json["access_token"]
-        self._refresh_token = json["refresh_token"]
+        ) as res:
+            print(await res.text())
+            res.raise_for_status()
+            json = await res.json()
+            self._access_token = json["access_token"]
+            self._refresh_token = json["refresh_token"]
         # TODO: Auto refresh using "expires_in"?
 
     async def _refresh_token(self):
-        res = await self._http_client.post(
+        async with self._http_session.post(
             self._token_url,
             params={
                 "refresh_token": self._refresh_token,
@@ -79,9 +111,9 @@ class GDriveDaemon(yaqd_core.BaseDaemon):
                 "client_secret": self._client_secret,
                 "grant_type": "refresh_token",
             },
-        )
-        res.raise_for_status()
-        self._access_token = res.json()["access_token"]
+        ) as res:
+            res.raise_for_status()
+            self._access_token = await res.json()["access_token"]
         # TODO: Auto refresh using "expires_in"?
 
     @refresh_oauth
@@ -99,3 +131,11 @@ class GDriveDaemon(yaqd_core.BaseDaemon):
     @refresh_oauth
     async def _generate_ids(self, count=128):
         pass
+
+if __name__ == "__main__":
+    GDriveDaemon.defaults.update({"client_id":"943700362860-7fmktmg2rjrblt4v2qh86141l6vg7qju.apps.googleusercontent.com", "client_secret":"pKIjNEasosRswlt4xWOxQCpD", "root_folder_id":"1oZOabPMoTO2XPE5mWOC_9XOR9PsUgepC"})
+    gdrive = GDriveDaemon("test",GDriveDaemon.defaults, "")
+    loop = asyncio.get_event_loop()
+
+    loop.run_until_complete(gdrive._authorize())
+
